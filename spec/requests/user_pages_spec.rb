5b71daef806627f196555a5caad8b67458d13648
Evan Dorfman <edorfs@gmail.com>
2012-07-14 16:35:26 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -43,5 +43,14 @@ describe "User pages" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
+
+      describe "after saving the user" do
+        before { click_button submit }
+          let(:user) { User.find_by_email('user@example.com') }
+
+          it { should have_selector('title', text: user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+          it { should have_link('Sign out') }
+      end
   end
 end
